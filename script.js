57982d28f286e9e131a172d52580aca25e479885
Fer <Hola@gmail>
2022-09-03 23:19:43 -0300

    iniciada la parte de JS

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function empezarJuego(){
     document.getElementById("objetoOculto1").style.display = "none"
     document.getElementById("objetoOcultoJuego").style.display = "block"
 };
+
+function pantallaInicio(){
+    document.getElementById("objetoOculto1").style.display = "flex"
+    document.getElementById("objetoOcultoJuego").style.display = "none"
+}
