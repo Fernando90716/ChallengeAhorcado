38cda53b5e4c7179b891d1b184e414b699e7d0aa
Fer <Hola@gmail>
2022-09-08 20:26:52 -0300

    Actualizando

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-function empezarJuego(){
-    document.getElementById("objetoOculto1").style.display = "none"
-    document.getElementById("objetoOcultoJuego").style.display = "block"
-};
+var palabrasJuego = ["hola", "nube", "gato", "perro", "cama"];
+var aciertos = 0;
+var fallos = 0;
+var nuevaPalabra = document.getElementById("escribirPalabra");
 
-function pantallaInicio(){
-    document.getElementById("objetoOculto1").style.display = "flex"
-    document.getElementById("objetoOcultoJuego").style.display = "none"
+
+function palabraRandom() {
+    
+}
+
+function empezarJuego()
+{
+    document.getElementById("menuPrincipal").style.display = "none";
+    document.getElementById("objetoOculto2").style.display = "block";
+    iniciarJuego() ;
+}
+
+function espacioOculto3() 
+{
+    document.getElementById("menuPrincipal").style.display = "none";
+    document.getElementById("espacioOculto3").style.display = "block";
+}
+
+function pantallaInicio()
+{
+    document.getElementById("menuPrincipal").style.display = "flex"
+    document.getElementById("objetoOculto2").style.display = "none"
+    document.getElementById("espacioOculto3").style.display = "none"
+}
+
+function iniciarJuego() 
+{
+    console.log("Hola");
+}
+
+function sumarPalabra(palabrita) {
+    palabrasJuego.push(palabrita);
+}
+
+function reiniciar()
+{
+    iniciarJuego();
+}
+
+document.addEventListener("keydown", mostrarTeclado);
+
+function mostrarTeclado(evento){
+    console.log(evento.keyCode);
+    /*numeros de identificacion 65 a 90 y */ 
 }
