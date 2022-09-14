a14798ec6d5decba26c700b3f502937a59e5dbea
Fer <Hola@gmail>
2022-09-14 20:47:50 -0300

    Mejorado el JS

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-var palabrasJuego = ["hola", "nube", "gato", "perro", "cama", "blanco", "negro", "azul"];
+const palabrasJuego = ["HOLA", "NUBE", "GATO", "PERRO", "CAMA", "BLANCO", "NEGRO", "AZUL","JAVA","SCRIPT","HTML","XBOX","WINDOWS","ALURA","FRONTEND","BACKEND","INTEL","AHORCADO","SECURITY"];
+
 var aciertos = 0;
 var fallos = 0;
+var letrasIngresadas = [];
+
+var letrasAcertadas = document.getElementById("letrasAcertadas");
+var palabrasEquivocadas = document.getElementById("palabrasEquivocadas");
 
 const palabraNueva = document.getElementById("escribirPalabra");
 
+var palabrita;
+
+function numeroAleatorio() {
+    var numberRandom = Math.floor(Math.random() * palabrasJuego.length);
+    return numberRandom;
+}
+
+function palabraObtenida(numberRandom)
+{
+    var numeroAleatorio = numberRandom;
+    var palabra = palabrasJuego[numeroAleatorio];
+    return palabra;
+}
 
 function sumarPalabra() {
-    insertarPalabraArray(palabraNueva.value);
+    insertarPalabraArray(palabraNueva.value.toUpperCase());
     palabraNueva.value = "";
     palabraNueva.focus();
 }
 
 function insertarPalabraArray(palabra) {
     palabrasJuego.push(palabra);
-}
-
-function palabraRandom() {
-    
 }
 
 function empezarJuego()
@@ -39,20 +53,93 @@ function pantallaInicio()
     document.getElementById("espacioOculto3").style.display = "none"
 }
 
-function iniciarJuego() 
+function keyFunction(event)
 {
-    console.log("Hola");
+	let spans = document.querySelectorAll("#spanLetras");
+	let recibeTecla = event.key;
+	let recibeCodigoTecla = event.keyCode;
+	let letraMayus = recibeTecla.toUpperCase();
+
+	if(recibeCodigoTecla >=65 && recibeCodigoTecla <= 90)
+	{
+		if(letrasIngresadas.includes(letraMayus))
+		{
+			return null;
+		}
+		else
+		{
+			letrasIngresadas.push(letraMayus);
+		}
+
+		let acerto = false; 
+		for(let i = 0; i < palabrita.length; i++)
+		{
+			if(letraMayus == palabrita[i]){
+				spans[i].innerHTML = letraMayus;
+				acerto = true; 
+				aciertos++;
+			}
+		}
+		if(acerto == false ){
+			let palabraEquivocadaDisplay = document.createElement("span");
+			palabraEquivocadaDisplay.classList.add("fallos");
+			palabraEquivocadaDisplay.innerHTML = letraMayus;
+			palabrasEquivocadas.appendChild(palabraEquivocadaDisplay);
+			fallos++;
+			let imagen = document.getElementById("graficaDibujo");
+		}
+
+		if(fallos === 9){
+		mensajeDerrota.style.visibility = "visible";
+		let palabraCorrecta = palabrita.join('');
+		mensajeDerrota.textContent = "Fin Del Juego !! la palabra era "+ palabraCorrecta ;
+		window.removeEventListener('keydown', keyFunction);
+		}
+		else if(aciertos == palabrita.length)
+		{
+			mensajeGanador.style.visibility = "visible";
+			window.removeEventListener('keydown', keyFunction);
+		}
+	}
+}
+
+function iniciarJuego() 
+{   
+
+    let recibePalabra = palabraObtenida(numeroAleatorio());
+    console.log(recibePalabra);
+	drawLines(recibePalabra);
+	window.addEventListener('keydown', keyFunction);
 }
 
 
 function reiniciar()
 {
-    iniciarJuego();
+	aciertos = 0;
+	fallos = 0;
+	letrasIngresadas = [];
+	graficaDibujo.style.visibility = "visible";
+	letrasAcertadas.style.visibility = "visible";
+	palabrasEquivocadas.style.visibility = "visible";
+	letrasAcertadas.innerHTML = "";
+	palabrasEquivocadas.innerHTML = "";
+	let recibePalabra = palabraObtenida(numeroAleatorio());
+	drawLines(recibePalabra);
+	window.addEventListener('keydown', keyFunction);
+    console.log(recibePalabra);
 }
 
-document.addEventListener("keydown", mostrarTeclado);
 
-function mostrarTeclado(evento){
-    console.log(evento.keyCode);
-    /*numeros de identificacion 65 a 90 y */ 
+function drawLines(palabra)
+{
+	palabrita = palabra.split("");
+
+	palabrita.forEach(element => {
+		let palabraDisplay = document.createElement("span");
+		palabraDisplay.classList.add("aciertos");
+		palabraDisplay.setAttribute("id","spanLetras");
+		letrasAcertadas.appendChild(palabraDisplay);
+	});
 }
+
+/*numeros de identificacion 65 a 90 y */
